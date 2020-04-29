c8516e4ad2cc652108b85b721f759ef4a198e1fa
Burak <burakalbayrak@live.com>
2020-04-29 23:38:47 +0300

    reorganizing attributes and adding new attributes (ADP, PROPN)

--- thales/thales/spacy.py
+++ thales/thales/spacy.py
@@ -7,12 +7,35 @@ def process(sentence):
 
     output = {
         "sentence": sentence,
-        "entities": [(ent.text, ent.start_char, ent.end_char, ent.label_) for ent in doc.ents],
-        "noun_chunks": [
-            (chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
-            for chunk in doc.noun_chunks
-        ],
-        "verbs": [token.lemma_ for token in doc if token.pos_ == "VERB"]
+        "entities": entities(doc.ents),
+        "noun_chunks": noun_chunks(doc.noun_chunks),
+        "ad_position": ad_position(doc),
+        "proper_names": proper_names(doc),
+        "verbs": verbs(doc)
     }
 
     return output
+
+
+def entities(entities):
+    return [(ent.text, ent.start_char, ent.end_char, ent.label_) for ent in entities]
+
+
+def noun_chunks(noun_chunks):
+    return [
+        (chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
+        for chunk in noun_chunks
+    ]
+
+
+def proper_names(doc):
+    return [(token.text, token.lemma_, token.tag_, token.dep_) for token in doc if token.pos_ == "PROPN"]
+
+
+def ad_position(doc):
+    return [(token.text, token.lemma_, token.tag_, token.dep_) for token in doc if token.pos_ == "ADP"]
+
+
+def verbs(doc):
+    return [(token.text, token.lemma_, token.tag_, token.dep_) for token in doc if
+            token.pos_ == "VERB" or token.pos_ == "AUX"]
